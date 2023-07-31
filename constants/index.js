f65db2b880b3956e617def7033b9327a4befb110
aliof11 <137686081+aliof11@users.noreply.github.com>
2023-07-31 23:30:10 +1300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbb025287c1d63e18c36868189bffa570439cfe20";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
